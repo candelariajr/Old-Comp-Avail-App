9c1cc5555e670afa56af99cdec0ee7f00d2fc696
Jonathan Candelaria <candelariajr@appstate.edu>
2022-07-07 01:24:37 -0400

    Update dbauth.php

--- dbauth.php
+++ dbauth.php
@@ -1,4 +1,5 @@
 <?php
+// THESE ARE DUMMY VALUES FOR A TEST BOX, DO NOT EVER USE ROOT OR LET YOUR AUTH GET EXPOSED ON THE WEB!!
 $user = "root";
 $password = "";
 $database = "computer_availability";
